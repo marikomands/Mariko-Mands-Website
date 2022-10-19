635c513eea992f8f80fc79b4b87b7f929114daaf
marikomands <marikomands@gmail.com>
2022-10-19 10:04:22 +0900

    my Skills

---
+++ src/components/Skills.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div>
+      <p>
+        I have studied programming for over a year. I have acquireed the
+        knowledge of HTML, CSS, React, React-Redux, React-form, React-Portal,
+        and Context System Baesed React.
+      </p>
+    </div>
+  );
+};
+
+export default Skills;
