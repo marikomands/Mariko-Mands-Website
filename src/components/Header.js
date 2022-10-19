871c3ebf278f36f31196853c86060287bb51eefe
marikomands <marikomands@gmail.com>
2022-10-19 14:45:40 +0900

    Added Links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import react from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   return (
-  <div>
-    <Link to="/"
-    
-    
-  </div>
-  )
+    <div className="header">
+      <Link to="/image-search">My Work with Hooks (Search Image App.)</Link>
+      <a href="https://github.com/marikomands">My Github</a>
+    </div>
+  );
 };
 
 export default Header;
