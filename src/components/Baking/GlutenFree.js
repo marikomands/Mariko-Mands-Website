1a038002c4b958fbce3c0b3615878d5eade256f0
marikomands <marikomands@gmail.com>
2022-10-19 10:04:10 +0900

    Gluten Free website

---
+++ src/components/Baking/GlutenFree.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const GlutenFree = () => {
+  return (
+    <div>
+      <h1>Gluten Free Baking</h1>
+      <p>
+        Gluten Free bread could be sometimes very hard and not fluffy like
+        regular bread. After experiencing many gluten free bread making, I have
+        came up with the recipies of the best gluten free bread. Also, I have
+        noticed many differencebetween Japanese and Western gluten free bread
+        recipies. In japan, we mainly use rice flour for gluten free bread (yes,
+        Japanese people love rice very much!) Unlike western recipies, the
+        recipies don't inclde any eggs. white eggs are full of protein and tend
+        to make a bread hard, so it might be because of that.
+      </p>
+    </div>
+  );
+};
+
+export default GlutenFree;
