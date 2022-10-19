c130c6395fa0a60d9bc9dd42e854fac976005cc1
marikomands <marikomands@gmail.com>
2022-10-19 10:04:55 +0900

    AboutMyself website

---
+++ src/components/AboutMyself.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AboutMyself = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        I was born in Gifu, Japan and grew up there until 16 years old. My
+        parents and I moved to Seattle, WA and I stayed there for over 8 years.
+        After going back to Japan, I worked in several differnt foregin
+        companies in Tokyo. After getting married to my husband, we lived in
+        Whitley Bay for less than a year and moved to London to start a new
+        life. Then, moved to Nanjing China to find a another new life. We are
+        blessed with wonderful 2 children now.
+      </p>
+    </div>
+  );
+};
+
+export default AboutMyself;
