3764d4e9d8ab5ae5a95e681cf9c7eed039f2e792
marikomands <marikomands@gmail.com>
2022-10-19 14:45:15 +0900

    added header and footer

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,18 @@
 import React from "react";
+// import { Router, Route, BrowserRouter } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Skills = () => {
   return (
     <div>
+      <Header />
       <p>
         I have studied programming for over a year. I have acquireed the
         knowledge of HTML, CSS, React, React-Redux, React-form, React-Portal,
         and Context System Baesed React.
       </p>
+      <Footer className="item" />
     </div>
   );
 };
