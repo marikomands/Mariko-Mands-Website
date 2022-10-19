5588dd160875f9f98ae89576780e6c3375b4047d
marikomands <marikomands@gmail.com>
2022-10-19 10:04:00 +0900

    WheatBased baking website

---
+++ src/components/Baking/WheatBased.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const WheatBased=()=>{
+return(
+  <div>
+<p>
+One of my hobbies is baking.  Especially, I like to bake bread.  It is always heaven to smell the freshly based bread.  
+I usually make Japanese style fluffy bread as my children like them.  They love Anpan which is round bread filled with read bean paste sesame seeds on top. 
+Some people say that kneading is too much trouble and not bothered to do it.  In my case, I don't feel the same as long as I have enough time for myself for that. 
+I enjoy the kneading process as a part of bread making.
+</p>
+  </div>
+)
+};
+
+
+export default WheatBased;
