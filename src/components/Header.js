e523326c17b4acaa3d6cc44982863066afd04fad
marikomands <marikomands@gmail.com>
2022-10-25 10:08:47 +0900

    added target attribute to show on different site

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,10 @@ const Header = () => {
   return (
     <div className="header">
       <Link to="/image-search">My Work with Hooks (Search Image App.)</Link>
-      <a href="https://github.com/marikomands">My Github</a>
+      <a href="https://github.com/marikomands" target="_blank">
+        My Github
+      </a>
+      <Link to="/calculator">React Calculator</Link>
     </div>
   );
 };
