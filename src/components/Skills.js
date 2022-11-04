ebc980bb154599f2695a233b900364e0ce612581
marikomands <marikomands@gmail.com>
2022-11-04 13:21:51 +0900

    added className "pContainer"

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,16 +2,18 @@ import React from "react";
 // import { Router, Route, BrowserRouter } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import "./Skills.css";
 
 const Skills = () => {
   return (
-    <div>
+    <div className="pContainer">
       <Header />
       <p>
         I have studied programming for over a year. I have acquireed the
         knowledge of HTML, CSS, React, React-Redux, React-form, React-Portal,
         and Context System Baesed React.
       </p>
+
       <Footer className="item" />
     </div>
   );
