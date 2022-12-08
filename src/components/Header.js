efee7c5c4d6e86e9d10b5802309763743d00742c
marikomands <marikomands@gmail.com>
2022-12-08 15:06:59 +0900

    added Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,20 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <Link className="link" to="/image-search">
-        My Work with Hooks (Search Image App.)
+      <Link className="linkHeader" to="/image-search">
+        Search Image App (API and Hooks)
       </Link>
-      <a className="link" href="https://github.com/marikomands" target="_blank">
+      <Link className="linkHeader" to="/clothing-list">
+        Clothes Shopping (API, Redux and Hooks)
+      </Link>
+      <a
+        className="linkHeader"
+        href="https://github.com/marikomands"
+        target="_blank"
+      >
         My Github
       </a>
-      <Link className="link" to="/calculator">
+      <Link className="linkHeader" to="/calculator">
         React Calculator
       </Link>
     </div>
