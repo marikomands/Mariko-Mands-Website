ef3db8a893e176b12da178d709d1bb671e9adb99
marikomands <marikomands@gmail.com>
2022-12-13 10:35:25 +0900

    Added to index.js

--- src/components/ClothingListRedux/index.js
+++ src/components/ClothingListRedux/index.js
@@ -2,16 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import App from "./components/App";
 import reducers from "./Reducers";
+import { Link } from "react-router-dom";
+import CategoryList from "./components/CategoryList";
+import TypeList from "./components/TypeList";
+import ItemList from "./components/ItemList";
+import "./App.css";
 
-const ClothesShopping = () => {
-  ReactDOM.render(
-    <Provider store={createStore(reducers)}>
-      <App />
-    </Provider>,
-    document.querySelector("#root")
+const App = () => {
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>Sulis</h2>
+      <div className="categoryTypeFlex">
+        <CategoryList />
+        <div className="itemFlex">
+          <ItemList />
+        </div>
+        <TypeList />
+      </div>
+      <div className="imageContainer">
+        <Link to="/" style={{ textDecoration: "underline" }}>
+          Home
+        </Link>
+      </div>
+    </div>
   );
 };
+
+const ClothesShopping = () => (
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>
+);
 
 export default ClothesShopping;
