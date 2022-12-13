fe023e605d6de828c248b13f435cafa5daeb9db5
marikomands <marikomands@gmail.com>
2022-12-13 10:35:43 +0900

    added CSS

--- src/components/ClothingListRedux/components/Item.js
+++ src/components/ClothingListRedux/components/Item.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./App.css";
+import "../App.css";
 
 const ClothesItem = ({ item }) => {
   const [imageSrc, setImageSrc] = useState(null);
